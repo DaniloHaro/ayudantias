c6fac452e7699922005d874a7cdb1fa3f1fa7ba3
DaniloHaro <danilo.haro@uchile.cl>
2025-07-22 17:11:53 -0400

    actualizacion 1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -112,3 +112,4 @@
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <!-- SweetAlert2 CDN -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <!-- mds -->
